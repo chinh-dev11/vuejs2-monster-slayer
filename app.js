a9e92792120dc8d74990d8c5264058ab52011259
Chinh Le <chinh.le11@gmail.com>
2019-06-25 17:40:56 -0400

    write better code - time for refactoring!

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 new Vue({
-    el: "#app",
-    data: {
+    el: "#app", // connect to DOM
+    // KIM: all usages of data property and methods in templates get re-rendered/executed at every data property changes       
+    data: { // store data to be used
         playerHealth: 100,
         monsterHealth: 100,
         isGameStarted: false,
+        damage: {
+            player: {
+                min: 5,
+                max: 12
+            },
+            monster: {
+                min: 3,
+                max: 10
+            },
+        },
         logs: []
     },
-    // KIM: computed (cached), if being used in templates, auto-executes once and gets re-executed ONLY if dependent data property changes
-    computed: {}, // dependent properties
-    // KIM: watch gets executed at every data property changes
-    watch: {}, // execute code upon data property changes
-    // KIM: methods, if being used in templates, get executed at every data property changes
+    // KIM: computed (cached), if being used in templates, gets executed ONLY if dependent data property changes
+    computed: {}, // dependent properties - synchronous tasks
+    // KIM: watch gets executed at every data property (counter1) changes - use as scarcely as possible (performance perspective)
+    watch: { // execute upon data changes - asynchronous tasks
+        // watchers are to be used as scarcely as possible - performance perspective
+        /* playerHealth: function (damage) {
+            if (damage <= 0) {
+                this.endGame('player');
+            }
+        },
+        monsterHealth: function (damage) {
+            if (damage <= 0) {
+                this.endGame('monster');
+            }
+        } */
+    },
     methods: { // methods of this Vue Instance
         startNewGame: function () {
             this.isGameStarted = true;
@@ -18,27 +40,15 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function () {
-            // console.log('attack');
-            var damageMax = 10;
-            var damageMin = 3;
-            // Math.random(): 0...1 (1 exclusive)
-            // Math.floor(): largest integer less or equal to given number
-            // Math.max(): largest of 0 or more numbers
-            var damage = Math.max(Math.floor(Math.random() * damageMax), damageMin);
-            this.monsterHealth -= damage;
-            if (this.monsterHealth <= 0) {
-                this.isGameStarted = false;
-                alert('You won!');
+            this.monsterHealth -= this.calculateDamage(this.damage.monster.min, this.damage.monster.max);
+            if (this.isGameEnded(this.monsterHealth)) {
+                this.endGame('monster');
                 return;
             }
 
-            damageMax = 12;
-            damageMin = 5;
-            damage = Math.max(Math.floor(Math.random() * damageMax), damageMin);
-            this.playerHealth -= damage;
-            if (this.playerHealth <= 0) {
-                this.isGameStarted = false;
-                alert('You lost!');
+            this.playerHealth -= this.calculateDamage(this.damage.player.min, this.damage.player.max);
+            if (this.isGameEnded(this.playerHealth)) {
+                this.endGame('player');
             }
         },
         specialAttack: function () {
@@ -52,6 +62,27 @@ new Vue({
         },
         addLog: function () {
             console.log('addLog');
+        },
+        calculateDamage: function (min, max) {
+            // Math.random(): 0...1 (1 exclusive)
+            // Math.floor(): largest integer less or equal to given number
+            // Math.max(): largest of 0 or more numbers
+            return Math.max(Math.floor(Math.random() * max), min);
+        },
+        isGameEnded: function (damage) {
+            if (damage <= 0) {
+                return true;
+            }
+
+            return false;
+        },
+        endGame: function (type) {
+            var msg = type === 'player' ? 'You lost!' : 'You won!';
+            if (confirm(msg + ' New game?')) {
+                this.startNewGame();
+            } else {
+                this.isGameStarted = false;
+            }
         }
     }
 });
